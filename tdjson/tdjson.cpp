9ddbf35815be8f13bf6d3c3f3610a09670f5e5e9
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2025-02-10 07:42:21 +0300

    Release GIL when calling TDLib functions to prevent blocking Python thread.

--- tdjson/tdjson.cpp
+++ tdjson/tdjson.cpp
@@ -1,11 +1,14 @@
 #include <td/telegram/td_json_client.h>
 #include <nanobind/nanobind.h>
 
+namespace nb = nanobind;
+using namespace nb::literals;
+
 NB_MODULE(tdjson_ext, m)
 {
-    m.def("td_create_client_id", &td_create_client_id, "Returns an opaque identifier of a new TDLib instance")
-        .def("td_send", &td_send, "Sends request to the TDLib client. May be called from any thread")
-        .def("td_receive", &td_receive, "Receives incoming updates and request responses. Must not be called simultaneously from two different threads")
-        .def("td_execute", &td_execute, "Synchronously executes a TDLib request");
+    m.def("td_create_client_id", &td_create_client_id, nb::call_guard<nb::gil_scoped_release>(), "Returns an opaque identifier of a new TDLib instance")
+        .def("td_send", &td_send, nb::call_guard<nb::gil_scoped_release>(), "Sends request to the TDLib client. May be called from any thread")
+        .def("td_receive", &td_receive, nb::call_guard<nb::gil_scoped_release>(), "Receives incoming updates and request responses. Must not be called simultaneously from two different threads")
+        .def("td_execute", &td_execute, nb::call_guard<nb::gil_scoped_release>(), "Synchronously executes a TDLib request");
     // TODO: td_set_log_message_callback?
 }
